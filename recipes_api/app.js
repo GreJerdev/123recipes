8a669f977126891a182b55bce3d264e605eaa6f3
Gregory Jerusalemsky <gregory.je@gmail.com>
2018-09-30 16:57:40 +0300

    rount updates

--- recipes_api/app.js
+++ recipes_api/app.js
@@ -1,6 +1,15 @@
+'use strict';
+
 const express = require('express')
 const app = express()
+const recipe = require('./controllers/recipe')
+const authentication = require('./controllers/authentication')
+const path = require("path")
 
-app.get('/', (req, res) => res.send('Hello World!'))
+console.log(__dirname);
+app.use('/public',express.static(path.join(__dirname, 'public')));
+app.use('/recipe', recipe);
+app.use('/authentication', authentication);
+app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000)
+app.listen(3000);
