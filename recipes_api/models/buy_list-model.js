681391a7025ba00a6c9b496139230a06d68729aa
Gregory Jerusalemsky <gregory.je@gmail.com>
2018-12-30 01:16:23 +0200

    added dc provider for buy items list

--- recipes_api/models/buy_list-model.js
+++ recipes_api/models/buy_list-model.js
@@ -1,13 +1,10 @@
 "use strict";
 
-
-module.exports = class buy_list{
-
-    constructor(){
-        this.id = 0
-        this.name = ""
-        this.description = ""
-        this.parent = 0
-
-    }
-}
+module.exports = class buy_list {
+  constructor() {
+    this.id = 0;
+    this.name = "";
+    this.description = "";
+    this.parent = 0;
+  }
+};
