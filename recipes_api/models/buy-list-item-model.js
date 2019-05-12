9e2b984cb933b83fa140fa82f89694a36e1b6635
grejerdev <gregory.je@gmail.com>
2019-05-12 22:11:01 +0300

    change buy list item properties

---
+++ recipes_api/models/buy-list-item-model.js
@@ -0,0 +1,20 @@
+"use strict";
+
+
+module.exports = class BuyListItem{
+
+    constructor(){
+        this.id = "";
+        this.buy_list_id = null;
+        this.note = "";
+        this.item_fulfitted = 0;
+        this.name = "";
+        this.price = 0;
+        this.units = 0;
+        this.units_type = "";
+        this.ingredient_id = null;
+        this.description = [];
+        this.image_url = "";
+    }
+};
+
